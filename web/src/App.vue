b73ae3133861b29ed8b72c21e9feab577888df7e
wangkewei <1067647932@qq.com>
2023-12-26 19:31:54 +0800

    init

--- web/src/App.vue
+++ web/src/App.vue
@@ -10,7 +10,16 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
+//@font-face {
+//    font-family: 'apple';  //重命名字体名
+//    src: url('@/style/font/pingfangsc-regular.otf');  //引入字体
+//}
+//
+//@font-face {
+//    font-family: 'digital';  //重命名字体名
+//    src: url('@/style/font/胡晓波男神体.otf');  //引入字体
+//}
 #app {
     width: 100%;
     height: 100%;
