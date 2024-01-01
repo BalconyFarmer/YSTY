5142ab4c8658cff6cac80cba71fb2e59227ec5b2
wangkewei <1067647932@qq.com>
2024-01-01 22:27:39 +0800

    api

--- webnew/src/App.vue
+++ webnew/src/App.vue
@@ -10,22 +10,13 @@ export default {
 }
 </script>
 
-<style lang="less">
-@import "./style/reset.less";
-@import "./style/elementPlusCustom.less";
+<style lang="scss">
+@import "./style/reset.scss";
+@import "./style/elementPlusCustom.scss";
 
 @font-face {
     font-family: 'apple';  //重命名字体名
     src: url('./style/font/pingfangsc-regular.otf');  //引入字体
 }
 
-#app {
-    width: 100%;
-    height: 100%;
-}
-
-html, body {
-    width: 100%;
-    height: 100%;
-}
 </style>
